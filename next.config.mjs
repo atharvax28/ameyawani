180d8f444c0b8acfdcb320d54fa28e2d2dea0f88
atharvax28 <athutayade@gmail.com>
2025-05-24 22:48:40 +0530

    Update basePath and assetPrefix for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/aakanksha-main' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/aakanksha-main/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/ameyawani' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ameyawani/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
